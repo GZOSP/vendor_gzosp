ed2c3d2a90052aa9f0fc3abc3073da84cd4d2df6
LuK1337 <priv.luk@gmail.com>
2020-09-01 11:56:43 +0200

    kernel: Use CPATH for including host headers
    * Fixes build on distros where CAP_LAST_CAP @ /usr/include/linux/capability.h
      doesn't match target device kernel src.
    
    Change-Id: Iafdf9694fd165c83b22bd95df95e940ecceb0fdd

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -118,7 +118,7 @@ endif
 ifeq ($(HOST_OS),darwin)
   KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I$(BUILD_TOP)/external/elfutils/libelf -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib"
 else
-  KERNEL_MAKE_FLAGS += HOSTCFLAGS="-I/usr/include -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -L/usr/lib64"
+  KERNEL_MAKE_FLAGS += CPATH="/usr/include:/usr/include/x86_64-linux-gnu" HOSTCFLAGS="-L/usr/lib/x86_64-linux-gnu -L/usr/lib64"
 endif
 
 TOOLS_PATH_OVERRIDE := LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib:$$LD_LIBRARY_PATH
